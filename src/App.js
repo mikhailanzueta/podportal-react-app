f574113a178c98e008d9ae44b1cd348d005d7ea1
Mikhail Anzueta <mikianzueta@gmail.com>
2024-07-01 10:42:21 -0400

    added .env file to .gitignore file.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import Discover from './components/discover'
 import Library from './components/library'
